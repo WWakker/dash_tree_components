d5fecc1ad2e8c4ed7d661332be9ff9991ae215c4
Wouter Wakker <wouter.wakker@outlook.com>
2026-05-22 18:52:56 +0200

    Version 1

--- src/DashTreeComponents.jl
+++ src/DashTreeComponents.jl
@@ -3,7 +3,7 @@ module DashTreeComponents
 using Dash
 
 const resources_path = realpath(joinpath( @__DIR__, "..", "deps"))
-const version = "0.0.8"
+const version = "1.0.0"
 
 include("jl/tree.jl")
 
